8d9de69a177c26282be5c6c95e2b521c357100e1
Mido Maverick <middle891129tw@gmail.com>
2025-12-03 11:06:09 +0800

    Allow initialization of FormworkLayerChecks

--- EngineeringFort/Formwork/FormworkDesigns.cs
+++ EngineeringFort/Formwork/FormworkDesigns.cs
@@ -18,7 +18,7 @@ public record class SideFormworkDesign : FormworkDesign, ISideFormworkDesign
     public virtual Pressure MaximumSidePressure { get; set; }
 
     [Display(Name = nameof(FormworkLayerCheck), ResourceType = typeof(DisplayStrings))]
-    public FormworkLayerCheck?[] FormworkLayerChecks { get; } = new FormworkLayerCheck?[5];
+    public FormworkLayerCheck?[] FormworkLayerChecks { get; init; } = new FormworkLayerCheck?[5]; // TODO: limit length
 
     public override IEnumerable<ICheck> SubChecks => FormworkLayerChecks.OfType<ICheck>();
 }
