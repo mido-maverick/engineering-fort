51702f3015876ac4f23bac8ae5936a5952a781b3
Mido Maverick <middle891129tw@gmail.com>
2025-12-11 13:50:37 +0800

    Add zero-check to Δmax to prevent division by zero

--- EngineeringFort/SteelConstructionManual.cs
+++ EngineeringFort/SteelConstructionManual.cs
@@ -38,8 +38,13 @@ public static class SteelConstructionManual
             {
                 public static Force Vmax(ForcePerLength w, Length l) => (w * l) / 2;
                 public static Torque Mmax(ForcePerLength w, Length l) => (w * (l * l)) / 8;
-                public static Length Δmax(ForcePerLength w, Length l, Pressure E, AreaMomentOfInertia I) => // (5 * w * (l * l * l * l)) / (384 * E * I)
-                    Length.FromMeters((5 * w.NewtonsPerMeter * Math.Pow(l.Meters, 4)) / (384 * E.Pascals * I.MetersToTheFourth));
+                public static Length Δmax(ForcePerLength w, Length l, Pressure E, AreaMomentOfInertia I) // (5 * w * (l * l * l * l)) / (384 * E * I)
+                {
+                    var eiNewtonSquareMeters = E.Pascals * I.MetersToTheFourth;
+                    if (eiNewtonSquareMeters is 0) return Length.Zero;
+
+                    return Length.FromMeters((5 * w.NewtonsPerMeter * Math.Pow(l.Meters, 4)) / (384 * eiNewtonSquareMeters));
+                }
             }
         }
 
@@ -49,8 +54,13 @@ public static class SteelConstructionManual
             {
                 public static Force Vmax(ForcePerLength w, Length l) => w * l;
                 public static Torque Mmax(ForcePerLength w, Length l) => (w * (l * l)) / 2;
-                public static Length Δmax(ForcePerLength w, Length l, Pressure E, AreaMomentOfInertia I) => // (w * (l * l * l * l)) / (8 * E * I)
-                    Length.FromMeters((w.NewtonsPerMeter * Math.Pow(l.Meters, 4)) / (8 * E.Pascals * I.MetersToTheFourth));
+                public static Length Δmax(ForcePerLength w, Length l, Pressure E, AreaMomentOfInertia I) // (w * (l * l * l * l)) / (8 * E * I)
+                { 
+                    var eiNewtonSquareMeters = E.Pascals * I.MetersToTheFourth;
+                    if (eiNewtonSquareMeters is 0) return Length.Zero;
+
+                    return Length.FromMeters((w.NewtonsPerMeter * Math.Pow(l.Meters, 4)) / (8 * eiNewtonSquareMeters));
+                }
             }
         }
 
@@ -62,8 +72,13 @@ public static class SteelConstructionManual
                 {
                     public static Force Vmax(ForcePerLength w, Length l) => 0.6 * (w * l);
                     public static Torque Mmax(ForcePerLength w, Length l) => 0.1 * (w * (l * l));
-                    public static Length Δmax(ForcePerLength w, Length l, Pressure E, AreaMomentOfInertia I) => // (w * (l * l * l * l)) / (145 * E * I)
-                        Length.FromMeters((w.NewtonsPerMeter * Math.Pow(l.Meters, 4)) / (145 * E.Pascals * I.MetersToTheFourth));
+                    public static Length Δmax(ForcePerLength w, Length l, Pressure E, AreaMomentOfInertia I) // (w * (l * l * l * l)) / (145 * E * I)
+                    {
+                        var eiNewtonSquareMeters = E.Pascals * I.MetersToTheFourth;
+                        if (eiNewtonSquareMeters is 0) return Length.Zero;
+
+                        return Length.FromMeters((w.NewtonsPerMeter * Math.Pow(l.Meters, 4)) / (145 * eiNewtonSquareMeters));
+                    }
                 }
             }
         }
