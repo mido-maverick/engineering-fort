06ad09e5511e5700a4126e0f13ec6e59458b5ab7
Mido Maverick <middle891129tw@gmail.com>
2025-12-12 16:51:06 +0800

    Add AllowableTensileForce to FormworkTieRod

--- EngineeringFort/Formwork/FormworkTieRod.cs
+++ EngineeringFort/Formwork/FormworkTieRod.cs
@@ -3,4 +3,6 @@ namespace EngineeringFort.Formwork;
 public record class FormworkTieRod : FormworkComponent
 {
     public override string Name { get => base.Name; set => base.Name = value; }
+
+    public virtual Force AllowableTensileForce { get; set; }
 }
